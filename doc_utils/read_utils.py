1b38f69c7f7a42b131e0c9b553dbd9aec9745be6
jacky2wong <jacky2wong@gmail.com>
2022-02-14 16:48:14 +1100

    update is_field

--- doc_utils/read_utils.py
+++ doc_utils/read_utils.py
@@ -2,47 +2,48 @@ import pandas as pd
 from typing import Dict, List, Any
 from .errors import MissingFieldError
 
+
 class DocReadUtils:
-    """This is created as a Mixin for others to easily add to their classes
-    """
+    """This is created as a Mixin for others to easily add to their classes"""
+
     @classmethod
-    def get_field(self, field: str, doc: Dict, missing_treatment: bool='raise_error'):
+    def get_field(self, field: str, doc: Dict, missing_treatment: bool = "raise_error"):
         """
-            For nested dictionaries, tries to access a field.
-            e.g. 
-            field = kfc.item
-            This should return "chickens" based on doc below.
-            {
-                "kfc": {
-                    "item": "chickens"
-                }
+        For nested dictionaries, tries to access a field.
+        e.g.
+        field = kfc.item
+        This should return "chickens" based on doc below.
+        {
+            "kfc": {
+                "item": "chickens"
             }
-            Args:
-                field:
-                    Field of a document.
-                doc: 
-                    document
-                missing_treatment:
-                    Can be one of return_empty_string/return_none/raise_error
-            Example:
-                >>> from vectorai.client import ViClient
-                >>> vi_client = ViClient(username, api_key, vectorai_url)
-                >>> sample_document = {'kfc': {'item': 'chicken'}}
-                >>> vi_client.get_field('kfc.item', sample_document) == 'chickens'
+        }
+        Args:
+            field:
+                Field of a document.
+            doc:
+                document
+            missing_treatment:
+                Can be one of return_empty_string/return_none/raise_error
+        Example:
+            >>> from vectorai.client import ViClient
+            >>> vi_client = ViClient(username, api_key, vectorai_url)
+            >>> sample_document = {'kfc': {'item': 'chicken'}}
+            >>> vi_client.get_field('kfc.item', sample_document) == 'chickens'
         """
         d = doc
         for f in field.split("."):
             try:
                 d = d[f]
             except KeyError:
-                try: 
+                try:
                     return doc[field]
                 except KeyError:
-                    if missing_treatment == 'return_none':
+                    if missing_treatment == "return_none":
                         return None
-                    elif missing_treatment == 'return_empty_string':
-                        return ''
-                    raise MissingFieldError("Document is missing " + f + ' of ' + field)
+                    elif missing_treatment == "return_empty_string":
+                        return ""
+                    raise MissingFieldError("Document is missing " + f + " of " + field)
 
             except TypeError:
                 if self._is_string_integer(f):
@@ -52,91 +53,106 @@ class DocReadUtils:
                     except IndexError:
                         pass
                 else:
-                    if missing_treatment == 'return_none':
+                    if missing_treatment == "return_none":
                         return None
-                    elif missing_treatment == 'return_empty_string':
-                        return ''
-                    raise MissingFieldError("Document is missing " + f + ' of ' + field)
+                    elif missing_treatment == "return_empty_string":
+                        return ""
+                    raise MissingFieldError("Document is missing " + f + " of " + field)
         return d
 
     @classmethod
     def _is_string_integer(cls, x):
-        """Test if a string is numeric
-        """
+        """Test if a string is numeric"""
         try:
             int(x)
             return True
         except:
             return False
 
-
     @classmethod
-    def get_fields(self, fields: List[str], doc: Dict, missing_treatment='return_empty_string') -> List[Any]:
+    def get_fields(
+        self, fields: List[str], doc: Dict, missing_treatment="return_empty_string"
+    ) -> List[Any]:
         """
-            For nested dictionaries, tries to access a field.
-            e.g. 
-            field = kfc.item
-            This should return "chickens" based on doc below.
-            {
-                "kfc": {
-                    "item": "chickens"
-                }
+        For nested dictionaries, tries to access a field.
+        e.g.
+        field = kfc.item
+        This should return "chickens" based on doc below.
+        {
+            "kfc": {
+                "item": "chickens"
             }
-            Args:
-                fields:
-                    List of fields of a document.
-                doc: 
-                    document
-            Example:
-                >>> from vectorai.client import ViClient
-                >>> vi_client = ViClient(username, api_key, vectorai_url)
-                >>> sample_document = {'kfc': {'item': 'chicken'}}
-                >>> vi_client.get_field('kfc.item', sample_document) == 'chickens'
+        }
+        Args:
+            fields:
+                List of fields of a document.
+            doc:
+                document
+        Example:
+            >>> from vectorai.client import ViClient
+            >>> vi_client = ViClient(username, api_key, vectorai_url)
+            >>> sample_document = {'kfc': {'item': 'chicken'}}
+            >>> vi_client.get_field('kfc.item', sample_document) == 'chickens'
         """
         return [self.get_field(f, doc, missing_treatment) for f in fields]
 
-    def get_field_across_documents(self, field: str, docs: List[Dict], missing_treatment: str='return_empty_string'):
+    def get_field_across_documents(
+        self,
+        field: str,
+        docs: List[Dict],
+        missing_treatment: str = "return_empty_string",
+    ):
         """
-            For nested dictionaries, tries to access a field.
-            e.g. 
-            field = kfc.item
-            This should return "chickens" based on doc below.
-            {
-                "kfc": {
-                    "item": "chickens"
-                }
+        For nested dictionaries, tries to access a field.
+        e.g.
+        field = kfc.item
+        This should return "chickens" based on doc below.
+        {
+            "kfc": {
+                "item": "chickens"
             }
-            Args:
-                fields:
-                    List of fields of a document.
-                doc: 
-                    document
-                missing_treatment:
-                    This can be one of 'skip', 'return_empty_string'
+        }
+        Args:
+            fields:
+                List of fields of a document.
+            doc:
+                document
+            missing_treatment:
+                This can be one of 'skip', 'return_empty_string'
 
-            Example:
-                >>> from vectorai.client import ViClient
-                >>> vi_client = ViClient(username, api_key, vectorai_url)
-                >>> documents = vi_client.create_sample_documents(10)
-                >>> vi_client.get_field_across_documents('size.cm', documents)
-                # returns 10 values in the nested dictionary
+        Example:
+            >>> from vectorai.client import ViClient
+            >>> vi_client = ViClient(username, api_key, vectorai_url)
+            >>> documents = vi_client.create_sample_documents(10)
+            >>> vi_client.get_field_across_documents('size.cm', documents)
+            # returns 10 values in the nested dictionary
         """
         if missing_treatment == "skip":
             # Returns only the relevant documents
             def check_field_in_doc(doc):
                 return self.is_field(field, doc)
-            return [self.get_field(field, doc) for doc in filter(check_field_in_doc, docs)]
+
+            return [
+                self.get_field(field, doc) for doc in filter(check_field_in_doc, docs)
+            ]
         return [self.get_field(field, doc, missing_treatment) for doc in docs]
-    
-    def get_fields_across_document(self, fields: List[str], doc: Dict, 
-        missing_treatment='return_empty_string'):
+
+    def get_fields_across_document(
+        self, fields: List[str], doc: Dict, missing_treatment="return_empty_string"
+    ):
         """
         Get numerous fields across a document.
         """
-        return [self.get_field(f, doc, missing_treatment=missing_treatment) for f in fields]
+        return [
+            self.get_field(f, doc, missing_treatment=missing_treatment) for f in fields
+        ]
 
-    def get_fields_across_documents(self, fields: List[str], docs: List[Dict], 
-        missing_treatment='return_empty_string'):
+    def get_fields_across_documents(
+        self,
+        fields: List[str],
+        docs: List[Dict],
+        missing_treatment="return_empty_string",
+    ):
         """Get numerous fields across documents.
 
         Example
@@ -168,26 +184,29 @@ class DocReadUtils:
         """
         if missing_treatment == "skip_if_any_missing":
             docs = self.filter_docs_for_fields(fields, docs)
-            return [self.get_fields_across_document(
-                fields, doc
-            ) for doc in docs]
-        return [self.get_fields_across_document(
-            fields, doc, missing_treatment=missing_treatment)
-            for doc in docs]
+            return [self.get_fields_across_document(fields, doc) for doc in docs]
+        return [
+            self.get_fields_across_document(
+                fields, doc, missing_treatment=missing_treatment
+            )
+            for doc in docs
+        ]
 
     def filter_docs_for_fields(self, fields: List, docs: List):
         """
         Filter for docs if they contain a list of fields
         """
+
         def is_any_field_missing_in_doc(doc):
             return all([self.is_field(f, doc) for f in fields])
+
         return filter(is_any_field_missing_in_doc, docs)
-        
+
     @classmethod
     def is_field(self, field: str, doc: Dict) -> bool:
         """
         For nested dictionaries, tries to access a field.
-        e.g. 
+        e.g.
         field = kfc.item
         This should return "chickens" based on doc below.
         {
@@ -198,7 +217,7 @@ class DocReadUtils:
         Args:
             collection_name:
                 Name of collection.
-            job_id: 
+            job_id:
                 ID of the job.
             job_name:
                 Name of the job.
@@ -212,12 +231,24 @@ class DocReadUtils:
         for f in field.split("."):
             try:
                 d = d[f]
-            except:
-                return False
+            except KeyError:
+                try:
+                    return doc[field]
+                except KeyError:
+                    return False
+            except TypeError:
+                if self._is_string_integer(f):
+                    # Get the Get the chunk document out.
+                    try:
+                        d = d[int(f)]
+                    except IndexError:
+                        pass
+                else:
+                    return False
         return True
 
     @staticmethod
-    def list_doc_fields(doc:dict)-> List[str]:
+    def list_doc_fields(doc: dict) -> List[str]:
         """returns all fields in a document, nested fields are flattened
         example:
         input: doc = {'a': {'b':'v', 'c':'v'},
@@ -225,50 +256,50 @@ class DocReadUtils:
                       'e':{'f':{'g':'v'}
         output: ['d', 'a.b', 'a.c', 'e.f.g']
         """
-        df = pd.json_normalize(doc, sep='.')
+        df = pd.json_normalize(doc, sep=".")
         return list(df.columns)
 
     @classmethod
     def subset_documents(
         self,
-        fields: List[str], 
-        docs: List[Dict], 
-        missing_treatment: str='return_none'
+        fields: List[str],
+        docs: List[Dict],
+        missing_treatment: str = "return_none",
     ) -> List[Dict]:
         """
-            Args:
-                fields:
-                    A list of fields of interest.
-                docs:
-                    A list of documents that may or may not have the chosen
-                    fields. 
-                missing_treatment:
-                    Cane be on of return_empty_string/return_none/raise_error
-            Example:
-                >>> from vectorai.client import ViClient
-                >>> vi_client = ViClient(username, api_key, vectorai_url)
-                >>> docs = [
-                ...     {"kfc": {"food": "chicken nuggets", "drink": "soda"}}
-                ...     {"mcd": {"food": "hamburger", "drink": "pop"}}
-                ... ]
-                >>> fields = [
-                ...     "kfc.food", "kfc.drink", "mcd.food", "mcd.drink"
-                ... ]
-                >>> vi_client.subset_documents(fields, docs) == [
-                ...     {
-                ...         "kfc.food": "chicken nuggets", "kfc.drink": "soda"},
-                ...         "mcd.food": "", "mcd.drink": ""
-                ...     },
-                ...     {
-                ...         "kfc.food": "", "kfc.drink": ""},
-                ...         "mcd.food": "hamburger", "mcd.drink": "pop"},
-                ...     }
-                ... ]
+        Args:
+            fields:
+                A list of fields of interest.
+            docs:
+                A list of documents that may or may not have the chosen
+                fields.
+            missing_treatment:
+                Cane be on of return_empty_string/return_none/raise_error
+        Example:
+            >>> from vectorai.client import ViClient
+            >>> vi_client = ViClient(username, api_key, vectorai_url)
+            >>> docs = [
+            ...     {"kfc": {"food": "chicken nuggets", "drink": "soda"}}
+            ...     {"mcd": {"food": "hamburger", "drink": "pop"}}
+            ... ]
+            >>> fields = [
+            ...     "kfc.food", "kfc.drink", "mcd.food", "mcd.drink"
+            ... ]
+            >>> vi_client.subset_documents(fields, docs) == [
+            ...     {
+            ...         "kfc.food": "chicken nuggets", "kfc.drink": "soda"},
+            ...         "mcd.food": "", "mcd.drink": ""
+            ...     },
+            ...     {
+            ...         "kfc.food": "", "kfc.drink": ""},
+            ...         "mcd.food": "hamburger", "mcd.drink": "pop"},
+            ...     }
+            ... ]
         """
         return [
             {
-                field: self.get_field(
-                    field, doc, missing_treatment=missing_treatment
-                ) for field in fields
-            } for doc in docs
+                field: self.get_field(field, doc, missing_treatment=missing_treatment)
+                for field in fields
+            }
+            for doc in docs
         ]
--- tests/test_read_utils.py
+++ tests/test_read_utils.py
@@ -1,27 +1,35 @@
 from doc_utils import DocUtils
 
+
 def test_is_field(sample_document):
     assert DocUtils().is_field("fehugieh", sample_document) is False
     key = list(sample_document.keys())[0]
     assert DocUtils().is_field(key, sample_document) is True
 
+
 def test_get_field_across_documents(sample_document, sample_2_document):
-    """Test to ensure you can get field across documents
-    """
+    """Test to ensure you can get field across documents"""
     sample_docs = [sample_document] * 20 + [sample_2_document] * 100
-    new_docs = DocUtils().get_field_across_documents("value", sample_docs, missing_treatment="skip")
+    new_docs = DocUtils().get_field_across_documents(
+        "value", sample_docs, missing_treatment="skip"
+    )
     assert len(new_docs) == 20, "Not skipping"
 
-def test_get_field_across_documents_for_skip_if_any_missing(sample_document, 
-    sample_2_document, combined_sample_document):
-    """Test to ensure you can get field across documents
-    """
-    sample_docs = [sample_document] * 20 + [sample_2_document] * 100 + \
-        [combined_sample_document] * 5
+
+def test_get_field_across_documents_for_skip_if_any_missing(
+    sample_document, sample_2_document, combined_sample_document
+):
+    """Test to ensure you can get field across documents"""
+    sample_docs = (
+        [sample_document] * 20
+        + [sample_2_document] * 100
+        + [combined_sample_document] * 5
+    )
     new_docs = DocUtils().get_fields_across_documents(
-        ["value", "check_value"], sample_docs,
-        missing_treatment="skip_if_any_missing")
+        ["value", "check_value"], sample_docs, missing_treatment="skip_if_any_missing"
+    )
     assert len(new_docs) == 5, "Not skipping"
+
 
 def test_subset_documents(combined_sample_document):
     sample_docs = [combined_sample_document] * 100
